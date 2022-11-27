281f7c822506afd95223d0037b50b17d47092fd4
Jo Seunghan <whtmdgks1120@gmail.com>
2022-11-27 14:38:42 +0900

    object detection example command

---
+++ detect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for id in 0 1 2 3
+do
+    python3 yolov5/detect.py \
+        --weights yolov5/yolov5s.pt \
+        --project runs/detect/simple \
+        --name cam$id \
+        --source data/simple/cam$id \
+        --img 640 --conf 0.25
+done
