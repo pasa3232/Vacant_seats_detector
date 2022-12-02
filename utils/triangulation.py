3b6b3da8b4a57330d290d48e6c1c06f7a5309342
Songmin17 <s.lee.0713@snu.ac.kr>
2022-12-02 23:42:24 +0900

    deleted utils jupyter nb and changed p3d

--- utils/triangulation.py
+++ utils/triangulation.py
@@ -9,18 +9,27 @@ def triangulation(poses, X, Y):
     ])
 
     constraint_mat = []
+    count = 0
     for pose, x, y in zip(poses, X, Y):
         calib = K @ pose
-        p1_t = calib[0, :]
-        p2_t = calib[1, :]
-        p3_t = calib[2, :]
+        if (count == 0):
+            print(f'calib:\n{calib}')
+            p1_t = calib[0, :]
+            p2_t = calib[1, :]
+            p3_t = calib[2, :]
+            print(f'p1_t:\n{p1_t}')
+            print(f'p2_t:\n{p2_t}')
+            print(f'p3_t:\n{p3_t}')
         constraint_mat.append([y * p3_t - p2_t])
         constraint_mat.append([x * p2_t - y * p1_t])
+        
+        count += 1
     constraint_mat = np.array(constraint_mat).reshape(1, 8, 4).squeeze()
 
     # svd for solution
     _, sigma , V = np.linalg.svd(constraint_mat[:6,:])
     vh = V[-1, :]
-    p3d = vh[:-1] / vh[-1]
+    p3d = vh / vh[-1]
+    # p3d = vh[:-1] / vh[-1]
 
     return p3d
