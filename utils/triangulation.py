baec5d0342b43a8f80814aacca7b09ae80ed6f04
Jo Seunghan <whtmdgks1120@gmail.com>
2022-12-03 00:28:14 +0900

    triangulation fix

--- utils/triangulation.py
+++ utils/triangulation.py
@@ -9,27 +9,23 @@ def triangulation(poses, X, Y):
     ])
 
     constraint_mat = []
-    count = 0
     for pose, x, y in zip(poses, X, Y):
+        pose = np.vstack((pose, np.array([0, 0, 0, 1])))
+        pose = np.linalg.inv(pose)
+        pose = pose[:3, :]
         calib = K @ pose
-        if (count == 0):
-            print(f'calib:\n{calib}')
-            p1_t = calib[0, :]
-            p2_t = calib[1, :]
-            p3_t = calib[2, :]
-            print(f'p1_t:\n{p1_t}')
-            print(f'p2_t:\n{p2_t}')
-            print(f'p3_t:\n{p3_t}')
+        p1_t = calib[0, :]
+        p2_t = calib[1, :]
+        p3_t = calib[2, :]
         constraint_mat.append([y * p3_t - p2_t])
         constraint_mat.append([x * p2_t - y * p1_t])
         
-        count += 1
-    constraint_mat = np.array(constraint_mat).reshape(1, 8, 4).squeeze()
+    constraint_mat = np.array(constraint_mat).reshape(1, 2 * len(poses), 4).squeeze()
 
     # svd for solution
-    _, sigma , V = np.linalg.svd(constraint_mat[:6,:])
+    _, sigma , V = np.linalg.svd(constraint_mat[:,:])
     vh = V[-1, :]
-    p3d = vh / vh[-1]
-    # p3d = vh[:-1] / vh[-1]
+    # p3d = vh / vh[-1]
+    p3d = vh[:-1] / vh[-1]
 
     return p3d
