ba50eacb80b5169f57a828036aeb5b53a30b0466
Taeksoo Kim <taeksu98@snu.ac.kr>
2022-12-04 16:55:45 +0900

    visualization for cam0

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,10 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 from camera_models import *
 
+K = np.array([
+        [975.813843, 0, 960.973816],
+        [0, 975.475220, 729.893921],
+        [0, 0, 1]
+    ])
 
-def show_world():
+
+# backprojects image pixel (a, b) to point on table surface (X, Y, Z)
+# Input: n pixels of dimension n x 2, camera pose (R | t) of dimension 3 x 4, plane coefficients (A, B, C, D)
+# output: n points on table surface in world coordinate of dimension n x 3 
+def pixel2surface(pixels, pose, plane_coeffs):
+    pixels, pose, plane_coeffs = np.array(pixels), np.array(pose), np.array(plane_coeffs)
+
+    pose = np.vstack((pose, np.array([0, 0, 0, 1])))
+    pose = np.linalg.inv(pose)
+    pose = pose[:3, :]
+
+    pixels_h = np.insert(pixels, pixels.shape[1], 1, axis=1)
+    back_projection = np.linalg.inv(K) @ pixels_h.T
+    b = np.vstack((np.zeros(pixels.shape[0]), back_projection))
+
+    A = np.vstack((plane_coeffs, pose))
+
+    points = np.linalg.inv(A) @ b
+    points = np.divide(points, points[-1])[:3]
+    return points.T
+
+
+
+def show_world(plane_coeffs=None, points=None):
     ### fetch camera poses
     num_cams = 4
     cam_poses = {} # key: cami, value: pose
@@ -74,19 +103,37 @@ def show_world():
     set_xyzlim3d(-3, 3)
     ax.set_title(f"World")
 
+    if plane_coeffs is not None:
+        x = np.linspace(-4, 4, 10)
+        y = np.linspace(-4, 4, 10)
 
-    x = np.linspace(-2, 2, 10)
-    y = np.linspace(-2, 2, 10)
+        A, B, C, D = plane_coeffs
+        x, y = np.meshgrid(x, y)
+        z = - 1 / C * (A * x + B * y + D)
 
-    A, B, C, D = np.array([0.04389121, -0.49583658, -0.25795586, 0.82805701])
-    x, y = np.meshgrid(x, y)
-    z = - 1 / C * (A * x + B * y + D)
+        surf = ax.plot_surface(x, y, z, alpha=0.2, linewidth=100)
 
-    surf = ax.plot_surface(x, y, z, alpha=0.2, linewidth=100)
+    if points is not None:
+        points = points[::10]
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='y', marker=".")
 
     plt.tight_layout()
     plt.show()
 
 
+
+
+
+
 if __name__ == "__main__":
-    show_world()
+    # show_world()
+    img = cv2.imread('./runs/discretize/cam0/discretized.jpg')
+    img = (np.round(img / 100) * 100).astype(np.uint8)
+    pixels = np.argwhere(((img[:,:,0] == 100) & (img[:,:,1] == 100) & (img[:,:,2] == 200))) # (b, a)
+    pose = np.zeros((3, 4))
+    pose[:3, :3] = np.eye(3)
+    plane_coeffs = np.array([0.04389121, -0.49583658, -0.25795586, 0.82805701])
+    points = pixel2surface(np.flip(pixels, axis=1), pose, plane_coeffs) # change pixels to (a, b)
+
+
+    show_world(plane_coeffs=plane_coeffs, points=points)
